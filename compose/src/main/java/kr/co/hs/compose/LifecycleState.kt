504c885a60d9e920ef49bba3d95a38b386287cb5
hsbaewa <hsbaewa@gmail.com>
2025-07-03 00:14:52 +0900

    0.0.9 - LifecycleState 에 collectAsState 함수 추가.

--- compose/src/main/java/kr/co/hs/compose/LifecycleState.kt
+++ compose/src/main/java/kr/co/hs/compose/LifecycleState.kt
@@ -2,6 +2,7 @@ package kr.co.hs.compose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -9,9 +10,41 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
+import kotlinx.coroutines.flow.StateFlow
 
+@Composable
+fun StateFlow<Lifecycle.State>.collectAsState(
+    onCreated: (Lifecycle.State) -> Unit = {},
+    onStarted: (Lifecycle.State) -> Unit = {},
+    onRestarted: (Lifecycle.State) -> Unit = {},
+    onResumed: (Lifecycle.State) -> Unit = {}
+): State<Lifecycle.State> {
+    val state = collectAsState()
+    var lastState by remember { mutableStateOf<Lifecycle.State?>(null) }
+    LaunchedEffect(state.value) {
+
+        when (state.value) {
+            Lifecycle.State.DESTROYED -> {}
+            Lifecycle.State.INITIALIZED -> {}
+            Lifecycle.State.CREATED -> onCreated(state.value)
+            Lifecycle.State.STARTED -> onStarted(state.value)
+            Lifecycle.State.RESUMED -> onResumed(state.value)
+        }
+
+        if (state.value.isAtLeast(Lifecycle.State.STARTED) && lastState == Lifecycle.State.CREATED) {
+            onRestarted(state.value)
+        }
+
+        lastState = state.value
+    }
+
+    return state
+}
+
+@Deprecated("use collectAsState(onCreated: onStarted: onRestarted: onResumed:)")
 @Composable
 fun LifecycleOwner.rememberLifecycleState(): LifecycleState {
     val lifecycleState = rememberSaveable(
@@ -45,6 +78,7 @@ fun LifecycleOwner.rememberLifecycleState(): LifecycleState {
     return lifecycleState
 }
 
+@Deprecated("use collectAsState(onCreated: onStarted: onRestarted: onResumed:)")
 class LifecycleState(
     isPaused: Boolean = false,
     isStarted: Boolean = false,
